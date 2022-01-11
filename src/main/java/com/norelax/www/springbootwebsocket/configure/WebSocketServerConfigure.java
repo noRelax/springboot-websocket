1b1c16ff8fdd4951937c466aefb6ee2f9c257c9b
wusong <wusong@8531.cn>
2022-01-11 20:24:06 +0800

    init commit

---
+++ src/main/java/com/norelax/www/springbootwebsocket/configure/WebSocketServerConfigure.java
@@ -0,0 +1,24 @@
+package com.norelax.www.springbootwebsocket.configure;
+
+import com.norelax.www.springbootwebsocket.handler.MyStringWebSocketHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+/**
+ * @author wusong
+ * @create 2022-01-11 20:14
+ **/
+@Configuration
+@EnableWebSocket
+public class WebSocketServerConfigure implements WebSocketConfigurer {
+    @Autowired
+    private MyStringWebSocketHandler myStringWebSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(myStringWebSocketHandler, "/connect").withSockJS();
+    }
+}
